8d4af95491439406e7ed20cc0f5bcd208d5b89f5
igorb871 <josearcadio1@gmail.com>
2013-11-11 10:02:20 +0200

    new

---
+++ src/tap_owner.erl
@@ -0,0 +1,32 @@
+-module(tap_owner).
+-export([start/1]).
+
+
+
+start(Num)-> start(Num,0).
+	
+	
+start(Num,Num)->
+	%io:format("created ~p taps\n",[Num]),
+  	 loop();
+start(Num,I)->
+	Tap_num=I+1,
+	Tap_name="tap" ++ integer_to_list(Tap_num),
+	case global:whereis_name(list_to_atom(Tap_name)) of
+		undefined -> 
+			{ok,Tap}= tuncer:create(Tap_name, [tap, no_pi, {active, true}]),
+			global:register_name(list_to_atom(Tap_name),Tap),
+			unlink(Tap),
+			io:format("Created ~p\n",[Tap_name]);
+		_-> ok
+	end,
+	start(Num,I+1).
+	
+
+loop()->
+	receive 
+		{Pid,Tap} -> tuncer:controlling_process(Tap,Pid);
+		{restart,Num} -> start(Num);
+		_->ok
+	end,
+	loop().
